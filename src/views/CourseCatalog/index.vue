8b723763570617e6dd4e5a9e9d06a5f415203d6a
tz_lws <tz_lws@163.com>
2021-04-27 19:12:05 +0800

    3.1

--- src/views/CourseCatalog/index.vue
+++ src/views/CourseCatalog/index.vue
@@ -47,13 +47,7 @@
           <el-dialog
             :visible.sync="dialogVisible"
             width="100%">
-            <video
-              style="width: 100%; height: 100%"
-              autoplay
-              playsinline
-              controls
-              :src='url'
-            ></video>
+            <div id="MPSplayer"></div>
           </el-dialog>
         </div>
         <div class="comments" v-else>
@@ -141,7 +135,30 @@
       },
       play(url) {
         this.dialogVisible = true
-        this.url = url
+        const this_ = this
+        this.player(url)
+        if($('#MPSplayer').length == 0) {
+          setTimeout(function(){this_.player(url)}, 1000)
+        }
+      },
+      player(url) {
+        // eslint-disable-next-line
+        new mpsPlayer({
+            container:'MPSplayer',//播放器容器ID，必要参数
+            uin: '1011',//用户ID
+            appId: 'fumBub1N6zNQxQUx',//播放实例ID
+            url,//控制台开通的hls/mp4地址
+            width: '100%',//播放器宽度，可用数字、百分比等
+            height: '100%',//播放器高度，可用数字、百分比等
+            autostart: true,//是否自动播放，默认为false
+            controlbardisplay: 'enable',//是否显示控制栏，值为：disable、enable默认为disable。
+            isclickplay: false,//是否单击播放，默认为false
+            isfullscreen: true,//是否双击全屏，默认为true
+            mobilefullscreen: false,//移动端是否全屏，默认为false
+            enablehtml5: true,//是否优先使用H5播放器，默认为false
+            ieUseH5:true,//boolean ie下是否使用H5（支持win10下ie（ie11）和edge） 默认false
+            isloadcount: 1//网络波动卡顿loading图标显示(默认1s后) 
+        });
       },
       assess(id, parentProjectId) {
         const data = {
@@ -366,6 +383,11 @@
 
           .el-dialog__body {
             height: 90%;
+
+            #MPSplayer {
+              width: 100%;
+              height: 100%;
+            }
           }
         }
       }
--- src/views/Courseware/index.vue
+++ src/views/Courseware/index.vue
@@ -85,7 +85,7 @@
           <template slot-scope="scope">
             <el-button type="text" size="mini" @click="play(scope.row.urlAddr)">播放</el-button>
             <el-button type="text" size="mini" @click="modify(scope.row.id, scope.row.ncesId, scope.row.subjectId)" :disabled='scope.row.ncesId ? false : true'>修改</el-button>
-            <el-button type="text" size="mini" @click="play(scope.row.urlVideo)">成果</el-button>
+            <el-button type="text" size="mini" @click="play(scope.row.urlVideo)" v-if="scope.row.urlVideo">成果</el-button>
             <el-button type="text" size="mini" @click="line(scope.row.id, scope.row.status)" :disabled='scope.row.finishStatus ? false : true'>{{ scope.row.status ? '下线' : '上线' }}</el-button>
             <el-button type="text" size="mini" @click="del(scope.row.id, scope.row.projectId, scope.row.subjectId)" :disabled='scope.row.status ? true : false'>删除</el-button>
           </template>
@@ -127,13 +127,7 @@
         class="play"
         :visible.sync="dialogVisible"
         width="100%">
-        <video
-          style="width: 100%; height: 100%"
-          autoplay
-          playsinline
-          controls
-          :src="url"
-        ></video>
+        <div id="MPSplayer"></div>
       </el-dialog>
     </div>
     <div class="bottom">
@@ -222,7 +216,30 @@
       },
       play(url) {
         this.dialogVisible = true
-        this.url = url
+        const this_ = this
+        this.player(url)
+        if($('#MPSplayer').length == 0) {
+          setTimeout(function(){this_.player(url)}, 1000)
+        }
+      },
+      player(url) {
+        // eslint-disable-next-line
+        new mpsPlayer({
+            container:'MPSplayer',//播放器容器ID，必要参数
+            uin: '1011',//用户ID
+            appId: 'fumBub1N6zNQxQUx',//播放实例ID
+            url,//控制台开通的hls/mp4地址
+            width: '100%',//播放器宽度，可用数字、百分比等
+            height: '100%',//播放器高度，可用数字、百分比等
+            autostart: true,//是否自动播放，默认为false
+            controlbardisplay: 'enable',//是否显示控制栏，值为：disable、enable默认为disable。
+            isclickplay: false,//是否单击播放，默认为false
+            isfullscreen: true,//是否双击全屏，默认为true
+            mobilefullscreen: false,//移动端是否全屏，默认为false
+            enablehtml5: true,//是否优先使用H5播放器，默认为false
+            ieUseH5:true,//boolean ie下是否使用H5（支持win10下ie（ie11）和edge） 默认false
+            isloadcount: 1//网络波动卡顿loading图标显示(默认1s后) 
+        });
       },
       modify(id, ncesId, subjectId) {
         const data = {
@@ -466,6 +483,11 @@
 
           .el-dialog__body {
             height: 90%;
+
+            #MPSplayer {
+              width: 100%;
+              height: 100%;
+            }
           }
         }
       }
